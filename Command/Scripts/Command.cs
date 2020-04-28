985135e3409b5cd0425ab2213b7c4a3f63eb512c
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2020-04-28 18:51:42 +0200

    Added the Command class

---
+++ Command/Scripts/Command.cs
@@ -0,0 +1,18 @@
+﻿namespace CM.Patterns.Command
+{
+	/// <summary>
+	/// This class represents functionality that can be executed.
+	/// </summary>
+	public abstract class Command
+	{
+		/// <summary>
+		/// Executes the functionality.
+		/// </summary>
+		public abstract void Execute();
+
+		/// <summary>
+		/// Undoes the previously executed functionality.
+		/// </summary>
+		public abstract void Undo();
+	}
+}
